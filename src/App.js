78745364daafe0ee90ba81ececb07be0a6ec045b
tinkugupta5 <36808633+tinkugupta5@users.noreply.github.com>
2023-05-06 17:51:58 +0530

    first commit

--- src/App.js
+++ src/App.js
@@ -1,24 +1,51 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.scss";
+import Box from "@mui/material/Box";
+import TextField from "@mui/material/TextField";
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <Box component="form" noValidate autoComplete="off">
+      <div className="App">
+        <div className="container">
+          <div className="card_area">
+            <h1 className="container_title">Back to your digital life</h1>
+            <p className="container_para">Choose one of the option to go</p>
+            <div className="input_card">
+              <TextField
+                className="input"
+                id="outlined-basic"
+                label="Email"
+                variant="outlined"
+              />
+              <TextField
+                className="input"
+                id="outlined-basic"
+                type="password"
+                label="Password"
+                variant="outlined"
+              />
+            </div>
+            <div className="line">
+              <p className="line_para">Or continue with</p>
+            </div>
+            <div className="social_card">
+              <div className="google">
+                <img className="google_img" alt="" src="/1.png" />
+              </div>
+              <div className="google">
+                <img className="google_img" alt="" src="/2.png" />
+              </div>
+              <div className="google">
+                <img className="google_img" alt="" src="/3.png" />
+              </div>
+            </div>
+            <div className="card_button">
+              <button class="button-28">Log in</button>
+            </div>
+          </div>
+        </div>
+      </div>
+    </Box>
   );
 }
 
